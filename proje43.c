e230084b24e0848fad30a201211462600bffd2ca
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:26:20 +0300

    sum

---
+++ proje43.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	//int dizi [2][2];
+	
+	//dizi [0][0]=10;
+	//dizi [0][1]=20;
+	//dizi [1][0]=30;
+	//dizi [1][1]=40;
+	
+	//printf("Matrisin 0-0 bulunan elemani: %d\n",dizi[0][0]);
+	//printf("Matrisin 0-1 bulunan elemani: %d\n",dizi[0][1]);
+	//printf("Matrisin 1-0 bulunan elemani: %d\n",dizi[1][0]);
+	//printf("Matrisin 1-1 bulunan elemani: %d\n",dizi[1][1]);
+	
+	
+	int dizi [2][2];
+	
+	dizi [0][0]=10;
+	dizi [0][1]=20;
+	dizi [1][0]=30;
+	dizi [1][1]=40;
+	
+	printf("%d ",dizi[0][0]);
+	printf("%d\n",dizi[0][1]);
+	printf("%d",dizi[1][0]);
+	printf(" %d\n",dizi[1][1]);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	return 0;
+}
